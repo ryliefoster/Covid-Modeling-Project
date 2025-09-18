62dc801794a31a1498f5e480cf302070c45fa4af
Rylie Foster <ryliefoster@gmail.com>
2025-09-18 09:00:56 -0700

    added some classes

--- main.py
+++
@@ -1,5 +0,0 @@
-class Person:
-    def __init__(self, status, group):
-        self.status = status
-        self.group = group
-    
---
+++ social_units.py
@@ -0,0 +1,23 @@
+class Person:
+    def __init__(self, id, infStatus, succeptibility):
+        self.id = id
+        self.infStatus = infStatus
+        self.succeptibility = succeptibility
+
+
+class Grouping:
+    def __init__(self, members):
+        self.members = members
+
+
+class Population:
+    def __init__(self, members):
+        self.members = members
+
+
+class SuperPop:
+    def __init__(self, pops):
+        self.pops = pops   
+
+
+
