cd355d8a7bcf7c275a9d7d2d33cea32a2e506ceb
Rylie Foster <ryliefoster@gmail.com>
2025-09-18 09:14:06 -0700

    idk wrote a bit

---
+++ interactions.py
@@ -0,0 +1,8 @@
+class Interaction:
+    def __init__(self, participants, precautions, duration):
+        self.participants = participants
+        self.precautions = precautions
+        self.duration = duration
+
+    def generateInfections(self):
+        pass
---
+++ main.py
@@ -0,0 +1,8 @@
+import social_units as su
+import interactions as intr
+
+
+
+# Step 1: Generate populations (staff, non-staff, etc.)
+
+#   
