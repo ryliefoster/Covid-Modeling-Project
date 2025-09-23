7e1deaecc1115b2edab92014a4be39b72a18b9c0
Nodoka Masamune <nmasamune9174@sdsu.edu>
2025-09-23 10:29:21 -0700

    new infection.py

---
+++ infection.py
@@ -0,0 +1,9 @@
+#infection.py
+
+'''
+Parameters: people (np.array, shape (N, ))
+    age, length of interaction, infection, precautions
+
+
+Outputs: update people's infection probability based on f(parameters)
+'''
