2fd9ccd5d6bc8b679dffc0c50dceefec16f85831
Rylie Foster <ryliefoster@gmail.com>
2025-11-13 10:45:50 -0800

    wrote stuff

--- social_units.py
+++ social_units.py
@@ -1,8 +1,14 @@
+import random
+import interactions
+import covidConstants as cc
+
 class Person:
-    def __init__(self, id, infStatus, susceptibility):
+    def __init__(self, id, status, susceptibility):
         self.id = id
-        self.infStatus = infStatus
+        self.status = status
         self.susceptibility = susceptibility
+        self.daysExposed = 0
+        self.daysInfected = 0
 
     def __str__(self):
         return(f"{str(self.id)} {str(self.infStatus)} {str(self.susceptibility)}")
@@ -13,22 +19,62 @@ class Person:
     def getInfStatus(self):
         return self.infStatus
     
+    def setInfStatus(self, status):
+        self.infStatus = status
+    
     def getSusceptibility(self):
         return self.susceptibility
-
-class Grouping:
-    def __init__(self, members):
-        self.members = members
+    
+    def calculateExposureProbablity(self, maxInteractions, infected, total):
+        if self.status == "S":
+            interactions = random.randint(0, maxInteractions)
+            exposureExents = interactions * infected / total
+            nonExposureProb = (1-cc.singleExposureProb)**exposureExents
+            return 1-nonExposureProb
+    
+    def updateStatus(self, exposureProb):
+        if self.status == 'S':
+            r = random.random()
+            if r<= exposureProb:
+                self.status = 'E'
+        if self.status == 'E':
+            self.daysExposed += 1
+            if self.daysExposed >= cc.incubationPeriod:
+                self.status = "I"
+        if self.status == 'I':
+            self.daysInfected += 1
+            if self.daysInfected >= cc.infectionDuration:
+                self.status = 'R'
 
 
 class Population:
-    def __init__(self, members):
+    def __init__(self, members, susceptible, exposed, infected, removed):
         self.members = members
+        self.susceptible = susceptible
+        self.exposed = exposed
+        self.infected = infected
+        self.removed = removed
 
+    def updateSEIR(self):
+        for m in self.members:
+            exposureProb = m.calculateExposure(len(self.members), self.infected, len(self.members))
+            m.updateStatus(exposureProb)
+        self.susceptible = 0
+        self.exposed = 0
+        self.infected = 0
+        self.infected = 0
+        for m in self.members:
+            if m.getStatus() == 'S':
+                self.susceptible += 1
+            if m.getStatus() == 'E':
+                self.exposed += 1
+            if m.getStatus() == 'I':
+                self.infected += 1
+            if m.getStatus() == 'R':
+                self.removed += 1
 
-class SuperPop:
-    def __init__(self, pops):
-        self.pops = pops   
+        
+
 
 
 
